616b4cd02c08b56e3714fc789b7d81b9f8a0fa8d
Sekai Kyoretsuna <sekai.kyoretsuna@gmail.com>
2016-04-01 13:51:16 -0400

    Add symbol table

---
+++ scorec/src/scorec/Compile/Analysis/SymbolTable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace ScoreC.Compile.Analysis
+{
+    using System.Diagnostics;
+    using SyntaxTree;
+
+    class Symbol
+    {
+        public string Name;
+        public TypeInfo TypeInfo;
+
+        public Symbol(string name, TypeInfo typeInfo)
+        {
+            Name = name;
+            TypeInfo = typeInfo;
+        }
+
+        public override string ToString() =>
+            Name + " : " + TypeInfo.ToString();
+    }
+
+    class SymbolTable
+    {
+        private Dictionary<string, Symbol> symbols = new Dictionary<string, Symbol>();
+
+        public bool IsDefined(string name)
+        {
+            // TODO(kai): this will need to change when overloading is a thing
+            return symbols.ContainsKey(name);
+        }
+
+        public void AddSymbol(string name, TypeInfo typeInfo)
+        {
+#if DEBUG
+            Debug.Assert(!IsDefined(name));
+#endif
+            symbols[name] = new Symbol(name, typeInfo);
+        }
+    }
+}
